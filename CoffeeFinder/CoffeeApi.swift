75c88f2d6489147e08558870cb04c677dff9f357
davidhsu1115 <davidhsu1115@gmail.com>
2016-05-15 05:02:26 +0800

    completed the coffee finder
    already finished the coffee finder.

---
+++ CoffeeFinder/CoffeeApi.swift
@@ -0,0 +1,9 @@
+//
+//  CoffeeApi.swift
+//  CoffeeFinder
+//
+//  Created by fangwiehsu on 2016/5/1.
+//  Copyright © 2016年 fangwiehsu. All rights reserved.
+//
+
+import Foundation
---
+++ CoffeeFinder/Venue.swift
@@ -0,0 +1,9 @@
+//
+//  Venue.swift
+//  CoffeeFinder
+//
+//  Created by fangwiehsu on 2016/5/9.
+//  Copyright © 2016年 fangwiehsu. All rights reserved.
+//
+
+import Foundation
